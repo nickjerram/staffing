ffcf0366edbf366213b4a2fcd9956857449daccd
nickjerram <nick.jerram@gmail.com>
2018-03-24 19:53:49 +0000

    live url

--- src/main/java/org/camra/staffing/data/repository/FormAreaRepository.java
+++ src/main/java/org/camra/staffing/data/repository/FormAreaRepository.java
@@ -3,5 +3,9 @@ package org.camra.staffing.data.repository;
 import org.camra.staffing.data.entity.FormArea;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FormAreaRepository extends JpaRepository<FormArea, Integer> {
+
+    List<FormArea> findAllByOrderById();
 }
